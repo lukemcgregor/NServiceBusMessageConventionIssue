87e63dcb95c70471c78608f0ace9f958e6bb13e1
Luke McGregor <luke@mcgregor.org.nz>
2014-08-19 19:01:39 +1200

    simplified

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -13,17 +13,9 @@ namespace ConsoleApplication1
 		{
 			Configure.With()
 				.DefineEndpointName("my.service.input")
-				.DefiningCommandsAs(t => t.Namespace != null && t.Namespace.Contains(".Commands"))
-				.DefiningEventsAs(t => t.Namespace != null && t.Namespace.Contains(".Events"))
 				.DefiningMessagesAs(t => t.Namespace != null && t.Namespace.Contains(".Messages"))
 				.DefaultBuilder()
-				.UseTransport<Msmq>()
-					.PurgeOnStartup(false)
-				.MsmqSubscriptionStorage("my.service")
 				.UnicastBus()
-					.LoadMessageHandlers()
-					.ImpersonateSender(false)
-				.EnablePerformanceCounters()
 				.CreateBus()
 				.Start(() => Configure.Instance.ForInstallationOn<NServiceBus.Installation.Environments.Windows>().Install());
 		}
